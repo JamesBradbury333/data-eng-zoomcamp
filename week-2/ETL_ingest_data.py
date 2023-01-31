3849abbd54b4471deff14244296cb15b04e45599
jamesbradbury333 <jamesbradbury222@gmail.com>
2023-01-31 23:42:11 +0000

    Finish week-2/ETL_ingest_data.py so that it runs as a single ETL. Works fine but canbe a bit memory heavy if you are using an old pc.

--- week-2/ETL_ingest_data.py
+++ week-2/ETL_ingest_data.py
@@ -4,9 +4,13 @@
 # (similar to airflow and other such tools)
 
 import pandas as pd
+from sqlalchemy import create_engine
 from prefect import flow, task
+import psycopg2
+import os
 
-
+# TODO: Set up a logger
+# TODO: Look at reading in data in chunks but in a better way than week-1   
 @flow(name="Ingest Data")
 def main():
     user = "postgres"
@@ -14,24 +18,36 @@ def main():
     host = "localhost"
     port = "5432"
     db = "ny_taxi"
-    table_name = "yellow_taxi_trips"
-    csv_path = "../week-1/database/yellow_tripdata_2021-01.csv"
+    table_name = "yellow_taxi_trips-week2"
+    csv_path = "week-1/python_docker_sql/database/yellow_tripdata_2021-01.csv"
+    print(os.getcwd())
 
-    extract_data(csv_path)
+    db_engine_url = f"postgresql://{user}:{password}@{host}:{port}/{db}"
+    db_engine = create_engine(db_engine_url)
+    data = extract_data(csv_path)
+    transformed_data = transform_data(data)
+    load_data(db_engine, table_name, transformed_data)
+    print("ETL job completed successfully!")
 
 
 @task(name="Extract-Taxi-Data", log_prints=True, retries=3)
 def extract_data(csv_path):
+    print(f"Loading file: {csv_path}")
     df = pd.read_csv(csv_path)
-    pass
+    return df
 
+@task(name="Transform-Taxi-Data", log_prints=True, retries=3)
+def transform_data(df: pd.DataFrame):
+    print("Converting datetime columns to datetime types")
+    df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
+    df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+    return df
 
-def transform_data():
-    pass
-
-
-def load_data():
-    pass
+@task(name="Load-Taxi-Data", log_prints=True, retries=3)
+def load_data(db_engine, db_tablename: str, df: pd.DataFrame):
+    print("Loading data into db...")
+    df.to_sql(name=db_tablename, con=db_engine, if_exists="replace")
+    print("Data loaded into db")
 
 
 if __name__ == "__main__":
